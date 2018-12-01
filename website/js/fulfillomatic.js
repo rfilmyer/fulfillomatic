ba952bc8e4763ec30d354a2d5bdbafd1c7ea7331
Roger Filmyer <roger.filmyer@gmail.com>
2018-12-01 11:43:17 -0500

    code cleanup

--- website/js/fulfillomatic.js
+++ website/js/fulfillomatic.js
@@ -1,15 +1,15 @@
 const quoteFetch = fetch("quote")
-  .then(res => res.text());
+    .then(res => res.text());
 
 // S/O https://stackoverflow.com/a/16599668/2856889
 function getLines(ctx, text, maxWidth) {
-    var words = text.split(" ");
-    var lines = [];
-    var currentLine = words[0];
+    const words = text.split(" ");
+    const lines = [];
+    let currentLine = words[0];
 
-    for (var i = 1; i < words.length; i++) {
-        var word = words[i];
-        var width = ctx.measureText(currentLine + " " + word).width;
+    for (let i = 1; i < words.length; i++) {
+        const word = words[i];
+        const width = ctx.measureText(currentLine + " " + word).width;
         if (width < maxWidth) {
             currentLine += " " + word;
         } else {
@@ -22,43 +22,42 @@ function getLines(ctx, text, maxWidth) {
 }
 
 function getTopHeights(numTextLines, textHeight, imageHeight) {
-  const centerLine = imageHeight / 2 - textHeight / 2;
-  const numLinesAboveCenterline = (numTextLines - 1) / 2
-  const textBoxTop = centerLine - numLinesAboveCenterline * textHeight
+    const centerLine = imageHeight / 2 - textHeight / 2;
+    const numLinesAboveCenterLine = (numTextLines - 1) / 2;
+    const textBoxTop = centerLine - numLinesAboveCenterLine * textHeight;
 
-  const lineIndices = [...Array(numTextLines).keys()] // if numTextLines = 5, return [0, 1, 2, 3, 4]
-  const heights = lineIndices.map(i => textBoxTop + i * textHeight)
+    const lineIndices = [...Array(numTextLines).keys()]; // if numTextLines = 5, return [0, 1, 2, 3, 4]
 
-  return heights;
+    return lineIndices.map(i => textBoxTop + i * textHeight);
 }
 
 function drawText(text){
 
-  const textHeightPixels = 48;
-  const imageHeight = 650;
+    const textHeightPixels = 48;
+    const imageHeight = 650;
 
-  const ctx = quoteContext;
-  ctx.font = textHeightPixels + 'px cursive';
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'top';
-  ctx.fillStyle = 'steelblue';
+    const ctx = quoteContext;
+    ctx.font = textHeightPixels + 'px cursive';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'top';
+    ctx.fillStyle = 'steelblue';
 
-  const textLines = getLines(ctx, text, 650 - 20);
+    const textLines = getLines(ctx, text, 650 - 20);
 
-  const topHeights = getTopHeights(textLines.length, textHeightPixels, imageHeight);
+    const topHeights = getTopHeights(textLines.length, textHeightPixels, imageHeight);
 
-  for (var i = textLines.length - 1; i >= 0; i--) {
-    const currentLine = textLines[i];
-    const currentHeight = topHeights[i];
-    ctx.fillText(currentLine, 325, currentHeight);
-  }
+    for (let i = textLines.length - 1; i >= 0; i--) {
+        const currentLine = textLines[i];
+        const currentHeight = topHeights[i];
+        ctx.fillText(currentLine, 325, currentHeight);
+    }
 
 
 }
 
 const quoteImage = new Image(),
-  quoteCanvas = document.getElementById('quote-image'),
-  quoteContext = quoteCanvas.getContext('2d')
+    quoteCanvas = document.getElementById('quote-image'),
+    quoteContext = quoteCanvas.getContext('2d')
 ;
 
 quoteImage.src = 'https://www.google.nl/images/srpr/logo3w.png';
@@ -72,4 +71,4 @@ quoteImage.onload = function(){
         0, 0,     // Place the result at 0, 0 in the canvas,
         650, 650); // With as width / height: 100 * 100 (scale)
     quoteFetch.then(drawText)
-}
+};
